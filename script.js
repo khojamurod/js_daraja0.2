133742c0117c411ae5953abdddc4e0db0bdca86f
khojakapro <azam188003@gmail.com>
2020-12-12 00:38:10 +0500

    nmagap2

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 var x1 = prompt('Soni kriting: ')
 var y =prompt ('Darjasini kriting: ')
 var x 
-for( x = x1 ; y > 1 ; y--){
+if(y== 0){
+    alert(x+'ni 0chi darajasini bilmiman oka')
+}else if(y<0){
+    alert('Manfiy darajalarni bilmiman oka')
+}else{
+    for( x = x1 ; y > 1 ; y--){
     x = x * x1
 }
+
 alert(x);
+}
