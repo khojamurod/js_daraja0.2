91e7685099735f2a8c3c09467d64ba2faf47833a
khojakapro <azam188003@gmail.com>
2020-12-12 00:41:27 +0500

    qwer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
-var x1 = prompt('Soni kriting: ')
-var y =prompt ('Darjasini kriting: ')
+var x1 = +prompt('Soni kriting: ')
+var y = +prompt ('Darjasini kriting: ')
 var x 
 if(y== 0){
     alert(x+'ni 0chi darajasini bilmiman oka')
